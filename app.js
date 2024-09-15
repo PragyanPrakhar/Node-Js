1c77688eae81eb0c03a202f698b9ddcd920aeb1e
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-09-15 23:01:43 +0530

    commom js module and es6 module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var naam="Namaste Pragyan";
-
-var a =10;
-var b = 20;
-console.log(naam);
-console.log(a+b);
+require("./sum.js");
+// calculateSum(2,3);
+const {calculateSum,as}=require("./sum.js");
+var as=23;
+console.log(as,calculateSum(2,3));
+console.log("In app.js",as);
---
+++ sum.js
@@ -0,0 +1,6 @@
+console.log("SUM");
+var as=100;
+function calculateSum(a,b){
+    console.log(a+b);
+}
+module.exports={as,calculateSum};
