2b3a0efb45446a05b3c8656f0104ebce999df110
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-09-16 03:35:34 +0530

    Concept of sync,async and libUv

---
+++ sync.js
@@ -0,0 +1,11 @@
+console.log("Hello! Async");
+
+function mulFn(x, y) {
+    const result = x * y;
+    return result;
+}
+
+var a = 1078698;
+var b = 20986;
+var c = mulFn(a, b);
+console.log("Multiplication result is: ", c);
